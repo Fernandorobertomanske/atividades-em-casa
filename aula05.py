0e5db9bc55a46c8bc0f4d36737ae228b968b01c6
Fernandorobertomanske <fernando992500090@gmail.com>
2023-10-14 17:27:03 -0300

    subir atividades que o teacher solicitou

---
+++ aula05.py
@@ -0,0 +1,13 @@
+nome            = str (input("gigite seu nome"))
+sobrenome       = str (input("digite seu sobrenome"))
+idade           = int (input("digite sua idade"))
+cpf             = float(input("digite seu cpf"))
+salario         = float(input("digite seu salario"))
+
+soma = ('nome, sobenome, idade, cpf, salario')
+
+print ("o tipo primitivo da variavel nome é", type(nome))
+print ("o tipo primitivo da variavel sobrenome é", type(sobrenome))
+print ("o tipo primitivo da variavel idade é", type(idade))
+print ("o tipo primitivo davariavel cpf é", type(cpf))
+print ("o tipo primitivo da variavel salario é", type(salario))
